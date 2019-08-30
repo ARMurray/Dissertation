9cd5e6ddfa48867e34d27d73b58eb085f6bf990a
Andrew <ARMurray@live.unc.edu>
2019-08-30 11:07:13 -0400

    Major Updates in Converting H5 to mosaiced Rasters

---
+++ H5_2_GeoTIFF.R
@@ -0,0 +1,56 @@
+library(raster)
+library(gdalUtils)
+library(dplyr)
+#library(stringr)
+
+# Identify the folder which contains all of the SMAP data files (both .h5 and .xml files)
+folder <- here("SMAP_Downloads")
+
+# Identify the folder you want to write the output GeoTiffs to.
+outFolder <- here("SMAP_GeoTIFFs")
+
+# List the .h5 files (These are the raster without spatial extents)
+h5Files <- list.files(path = folder, pattern = ".h5$", recursive = TRUE)
+
+# List the .xml files (These have the spatial extents for the rasters)
+xmlFiles <- list.files(path = here("SMAP_Downloads"), pattern = ".iso.xml$", recursive = TRUE)
+
+# Here we will iterate through the folder file by file and convert the seperate
+# H5 and xml files to singular GeoTIFFs
+
+start <- Sys.time()
+
+for(n in 1:length(h5Files)){
+  h5 <- paste0(folder,h5Files[n])
+  # Convert from H5 to GeoTiff and save it (This still won't have any spatial reference info)
+  gdal_translate(h5,paste0(outFolder,substr(h5Files[n],1,nchar(h5Files[n])-3),".tif"), of = "GTiff", sd_index = 86,verbose=TRUE, overwrite = TRUE)
+  # Import the GeoTIff and assign the correct projection (global cylindrical EASE-Grid 2.0 (Brodzik et al. 2012))
+  raster <- raster(paste0(outFolder,substr(h5Files[n],1,nchar(h5Files[n])-3),".tif"))
+  projection(raster) <- "+proj=cea +lon_0=0 +lat_ts=30 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+  # Import the xml file
+  xmlLines <- readLines(paste0(folder,xmlFiles[n]))
+  xmlString <- toString(xmlLines)
+  # Find the start of the extent coordinates
+  coordsStartLoc <- str_locate(xmlString,"srsName=\"http://www.opengis.net/def/crs/EPSG/4326\">")
+  coordsStartLoc <- coordsStartLoc[!is.na(coordsStartLoc)][2]
+  # Find the end of the extent coordinates
+  coordsEndLoc <- str_locate(xmlString,"</gml:posList>")
+  coordsEndLoc <- coordsEndLoc[!is.na(coordsEndLoc)][1]
+  # Extract the coordinates
+  coords <- substr(xmlString,coordsStartLoc+1,coordsEndLoc-1)
+  coords <- str_split(coords, " ", simplify = TRUE)
+  options(digits = 15)
+  xmin <- as.numeric(coords[2])
+  xmax <- as.numeric(coords[4])
+  ymin <- as.numeric(coords[5])
+  ymax <- as.numeric(coords[1])
+  # Project the raster to standard WGS84 so units are decimal degrees
+  wgs84 <- projectRaster(raster, crs = "+proj=longlat +datum=WGS84 +no_defs")
+  wgs84 <- setExtent(wgs84,extent(xmin,xmax,ymin,ymax))%>%
+    trim()
+  # Write the GeoTIFF with correct spatial extent
+  writeRaster(wgs84,paste0(outFolder,substr(h5Files[n],1,nchar(h5Files[n])-3),".tif"),overwrite=TRUE)
+}
+
+end <- Sys.time()
+print(paste0("Converted: ",length(h5Files)," files to GeoTiffs in: ",end-start))
---
+++ mosaic_overlapping_rasters.R
@@ -0,0 +1,51 @@
+library(raster)
+library(gdalUtils)
+library(here)
+library(dplyr)
+
+# Set the date you want to mosaic in the format: 'yyyymmdd'
+date <- '20180826'
+
+# SMAP images overlap eachother and also have a large number of NA values
+# In many cases, NA values from one raster overlap real values in another
+# raster. The function below will mosaic multiple rasters together but in cases
+# where pixels overlap will default to the higher number. NA values in SMAP come
+# as '-9999' so this allows NA values to be removed in favor of real values
+
+mosaicList <- function(rasList){
+  
+  #Internal function to make a list of raster objects from list of files.
+  ListRasters <- function(list_names) {
+    raster_list <- list() # initialise the list of rasters
+    for (i in 1:(length(list_names))){ 
+      grd_name <- list_names[i] # list_names contains all the names of the images in .grd format
+      raster_file <- raster::raster(grd_name)
+      raster_file <- projectRaster(raster_file, snap, method = "ngb")
+    }
+    raster_list <- append(raster_list, raster_file) # update raster_list at each iteration
+  }
+  
+  #convert every raster path to a raster object and create list of the results
+  raster.list <-sapply(rasList, FUN = ListRasters)
+  
+  # edit settings of the raster list for use in do.call and mosaic
+  names(raster.list) <- NULL
+  #####This function deals with overlapping areas
+  raster.list$fun <- max
+  #raster.list$tolerance <- 0.1
+  
+  #run do call to implement mosaic over the list of raster objects.
+  mos <- do.call(raster::mosaic, raster.list)
+  
+  #set crs of output
+  crs(mos) <- crs(x = raster(rasList[1]))
+  return(mos)
+}
+
+raster_files <- list.files(path =here("SMAP_GeoTIFFs"),pattern = date,full.names = TRUE )
+snap <- raster(resolution = c(0.02839512,0.02433336), xmn = -120, xmx = -60, ymn = 0, ymx = 60, crs = "+proj=longlat +datum=WGS84 +no_defs") 
+mosaic_layer <- mosaicList(raster_files )%>%
+  trim()
+plot(mosaic_layer)
+
+writeRaster(mosaic_layer,paste0(here("SMAP_Mosaics_by_Date"),"/SMAP_Mosaic_",date,".tif"), overwrite = TRUE)
