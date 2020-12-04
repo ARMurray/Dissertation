115864d4157d5cb0695dc3b5b20ac6040ed776d2
Andrew <armurray@live.unc.edu>
2020-12-04 13:50:48 -0500

    AGU Dashboards working

--- Presentations/AGU_2020/Create_PPT_Dashboards.R
+++ Presentations/AGU_2020/Create_PPT_Dashboards.R
@@ -17,10 +17,18 @@ long <- sf%>%
   pivot_longer(cols = starts_with("al"), names_to = "Storm", values_to = "ppt_mm")%>%
   left_join(NOAA, by = c("Storm" = "ID"))%>%
   mutate(Name = ifelse(Storm == "allstorms","All Storms",Name),
-         nameYear = paste0(Name," (",substr(lFall_dateTime,1,4),")"))%>%
+         nameYear = paste0(Name," (",substr(lFall_dateTime,1,4),")"),
+         Storm = ifelse(Storm == "allstorms","allstorm",Storm),
+         Class = ifelse(ppt_mm <100,"< 100",
+                                 ifelse(ppt_mm < 250, "100 - 250",
+                                        ifelse(ppt_mm < 500, "250 - 500",
+                                               ifelse(ppt_mm < 750, "500 - 750",
+                                                      ifelse(ppt_mm < 1000, "750 - 1000",
+                                                             ifelse(ppt_mm > 1000,"> 1,000", NA)))))))%>%
   filter(!is.na(ppt_mm))%>%
   st_as_sf()
 
+pal <- c("#4FC3F8","#19A6D4","#1B5CB0","#8E4DC8","#FF25F5")
 
 library("rmarkdown")
 
@@ -30,7 +38,7 @@ library("rmarkdown")
 for (storm in unique(long$Storm)){
   subgroup <- long%>%
     filter(Storm == storm)
-  render(here("Presentations/AGU_2020/Cyclone_PPT_Template.Rmd"),output_file = here("Presentations/AGU_2020/dashboards",paste0(subgroup$Name,"_PPT",'.html')))    
+  render(here("Presentations/AGU_2020/Cyclone_PPT_Template.Rmd"),output_file = here("Presentations/AGU_2020/dashboards",paste0(subgroup$Name[1],"_PPT",'.html')))    
 
-  print(paste0("Finshed: ", id))  
+  print(paste0("Finshed: ", storm))  
 }
